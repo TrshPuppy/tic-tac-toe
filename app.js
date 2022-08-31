6aed994c6f7502338fc502c49c38a5435b7e51cc
Rose Pineau <rosepineau@protonmail.com>
2022-08-31 14:39:00 -0400

    Add createBoard().

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ let gameBoard = [
   ["", "", ""],
 ];
 
+const gameBoardUI = document.querySelector(".game-board");
+let gameTile;
+
+createBoard();
+
 let playerPieceChoice = prompt("choose a gamepiece");
 
 const player = createPlayer(playerPieceChoice);
@@ -15,4 +20,15 @@ function createPlayer(gamePiece) {
   return {
     gamePiece: gamePiece,
   };
+}
+
+function createBoard() {
+  gameBoardUI.style.gridTemplateColumns = "repeat(3,1fr)";
+  gameBoardUI.style.gridTemplateRows = "repeat(3,1fr)";
+
+  for (let i = 1; i < 10; i++) {
+    gameTile = document.createElement("div");
+    gameTile.setAttribute("data-number", i);
+    gameBoardUI.insertAdjacentElement("beforeend", gameTile);
+  }
 }
