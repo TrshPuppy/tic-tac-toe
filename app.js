4631ca49ad9383b87dff5be58f895fe188250426
Rose Pineau <rosepineau@protonmail.com>
2022-08-31 15:37:14 -0400

    Fix createBoard() to populate divs w/ array-index data.

--- app.js
+++ app.js
@@ -26,9 +26,31 @@ function createBoard() {
   gameBoardUI.style.gridTemplateColumns = "repeat(3,1fr)";
   gameBoardUI.style.gridTemplateRows = "repeat(3,1fr)";
 
-  for (let i = 1; i < 10; i++) {
+  for (let i = 0; i < 9; ++i) {
     gameTile = document.createElement("div");
-    gameTile.setAttribute("data-number", i);
+    gameTile.setAttribute(
+      "data-number",
+      `${Math.trunc(i / 3)},${i % 3}`
+      //   (gameBoard[Math.trunc(i / 3)][i % 3] = i);
+    );
     gameBoardUI.insertAdjacentElement("beforeend", gameTile);
   }
+
+  //   for (let i = 0; i < 3; i++) {
+  //     for (let j = 0; j < 3; j++) {
+  //       gameTile = document.createElement("div");
+  //       gameTile.setAttribute("data-number", i);
+  //       gameBoardUI.insertAdjacentElement("beforeend", gameTile);
+  //       gameTile.addEventListener("click", () => {
+  //         updateGameBoardArray("game time data number");
+  //         updateUI();
+  //       });
+  //     }
+  //   }
 }
+
+// updateGameBoardArray("game tile data number"){
+// }
+
+// updateUI(){
+// }
